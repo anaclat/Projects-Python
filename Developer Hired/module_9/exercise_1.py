34b04309bda2e42343b31f7f7c653fee017d7a68
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-06-13 21:47:50 +0000

    module_9 exercises

---
+++ Developer Hired/module_9/exercise_1.py
@@ -0,0 +1,27 @@
+serie_a = [
+    "Flamengo",
+    "Cruzeiro",
+    "Red Bull Bragantino",
+    "Palmeiras",
+    "Bahia",
+    "Fluminense",
+    "Atlético-MG",
+    "Botafogo",
+    "Mirassol",
+    "Corinthians",
+    "Grêmio",
+    "Ceará",
+    "Vasco da Gama",
+    "São Paulo",
+    "Santos",
+    "Vitória",
+    "Internacional",
+    "Fortaleza",
+    "Juventude",
+    "Sport"
+]
+
+print(f'O primeiro colocado é: {serie_a[0]}.')
+print(f'O segundo colocado é: {serie_a[-1]}.')
+print(f'Os 4 primeiros colocado são: {serie_a[:4]}.')
+print(f'Os 4 últimos colocados são: {serie_a[-4:]}.')
