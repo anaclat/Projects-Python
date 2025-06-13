24add469f082c99bf1054bc7a630d42bdfc9f554
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-06-13 21:01:35 +0000

    module_8_1

--- Developer Hired/module_8/exercise_1.py
+++ Developer Hired/module_8/exercise_1.py
@@ -1,1 +1,5 @@
+extenso  = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito')
+
 numero = int(input('Digite um número de 0 a 8: '))
+
+print(f'O número {numero} por extenso é: {extenso[numero]}.')
