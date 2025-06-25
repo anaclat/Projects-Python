1bd9519bb7ee80633e99ccb899a5a6272d3f00a4
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-06-25 14:15:31 +0000

    savepoint

--- Developer Hired/Chapter 2 (end)/module_12/exercise_16.py
+++ Developer Hired/Chapter 2 (end)/module_12/exercise_16.py
@@ -1,12 +1,18 @@
 from random import randint
 
+jogadas = ('PEDRA', 'PAPEL', 'TESOURA')
 computador = randint(0,2)
 
 print('''
 Vamos jogar:
-      [1] PEDRA
-      [2] PAPEL
-      [3] TESOURA
+      [0] PEDRA
+      [1] PAPEL
+      [2] TESOURA
    ''')
 
 usuario = int(input('Faça sua jogada: '))
+
+print(f'O computador jogou {jogadas[computador]} e você jogous {jogadas[usuario]}.')
+
+if computador == 0:
+    print(f'')
