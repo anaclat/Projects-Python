b60011e626bf6be5318f5f1cf5bcc183afc86613
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-06-25 17:57:24 +0000

    module 13 (exercises 1 and 2)

---
+++ Developer Hired/Chapter 2 (end)/module_13/#suport_material.py
@@ -0,0 +1,23 @@
+# WHILE
+
+#for numero in range(0,10):
+#    print('Ana Clara')
+
+#contador = 0
+
+#while contador < 10:
+#    print('Ana Clara')
+#    contador +=1
+
+#numero = int(input('Digite o número 8 ou 9: '))
+#while numero not in (8,9):
+#    print('Ainda não é o número que eu quero!')
+#    numero = int(input('Digite o número 8 ou 9: '))
+#print('Agora sim! Número correto!')
+
+numero = 1
+while True:
+    print('Para sempre...')
+
+    if numero == 1:
+     break
---
+++ Developer Hired/Chapter 2 (end)/module_13/exercise_1.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite o seu nome: '))
+
+contador = 0
+
+while contador < 10:
+    print(f'O seu nome é: {nome}.')
+    contador +=1
