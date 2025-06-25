e79c934cd79fcbfa92a16d7ad8d073c1bed7cf57
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-06-25 04:30:33 +0000

    savepoint exercise 16 (module 12)

---
+++ Developer Hired/Chapter 2 (end)/module_12/exercise_16.py
@@ -0,0 +1,12 @@
+from random import randint
+
+computador = randint(0,2)
+
+print('''
+Vamos jogar:
+      [1] PEDRA
+      [2] PAPEL
+      [3] TESOURA
+   ''')
+
+usuario = int(input('Faça sua jogada: '))
