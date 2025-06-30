4f82d3d05fd881d47ba3143053aacbb255a67cac
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-06-30 20:41:43 +0000

    att module 14

--- Developer Hired/Chapter 2 (end)/module_14/#suport_material.py
+++ Developer Hired/Chapter 2 (end)/module_14/#suport_material.py
@@ -1,6 +1,17 @@
 # função
 
-def printar_tracinhos():
+def printar_tracinhos(): # função sem parâmetro
     print('=========')
 
 printar_tracinhos()
+
+def escrever_nome_formatado(nome): # função com 1 parâmetro
+    print(f'Meu nome é {nome}.')
+
+escrever_nome_formatado('ana')
+
+def somar(numero_1, numero_2):
+    resultado = numero_1 + numero_2
+    print(resultado)
+
+somar(3,6)
