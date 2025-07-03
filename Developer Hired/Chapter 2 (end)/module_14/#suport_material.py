48db188b352e3e6ef06f3cfa07d910fc7120842c
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-07-03 21:15:06 +0000

    suport material and exercise 1 (module 14)

--- Developer Hired/Chapter 2 (end)/module_14/#suport_material.py
+++ Developer Hired/Chapter 2 (end)/module_14/#suport_material.py
@@ -5,13 +5,37 @@ def printar_tracinhos(): # função sem parâmetro
 
 printar_tracinhos()
 
+#########################################
 def escrever_nome_formatado(nome): # função com 1 parâmetro
     print(f'Meu nome é {nome}.')
 
 escrever_nome_formatado('ana')
 
-def somar(numero_1, numero_2):
+#########################################
+def somar(numero_1, numero_2): # função de 2 ou mais parâmetros
     resultado = numero_1 + numero_2
     print(resultado)
 
 somar(3,6)
+
+#########################################
+def saudar(nome,mensagem='Bom dia'): #função com parâmetro default
+    print(f'Olá, {nome}! {mensagem}')
+
+saudar('Ana')
+saudar('Ana', mensagem='Boa Tarde!')
+
+#########################################
+
+def calcular_media(nota_1,nota_2):
+    media = (nota_1 + nota_2)/2
+    return media
+
+def verificar(media):
+    if media >=7:
+        print(f'Você está APROVADO.')
+    else:
+        print(f'Você está REPROVADO.')
+
+media = calcular_media(9,4)
+verificar(media)
---
+++ Developer Hired/Chapter 2 (end)/module_14/exercise_1.py
@@ -0,0 +1,7 @@
+def funcao(numero_1,numero_2):
+    soma = numero_1 + numero_2
+    return soma
+
+soma = funcao(7,8)
+
+print(f'O resultado da soma é: {soma}.')
