35a20dad818190f1b355abaaf4b7459f62185fc9
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-07-06 01:18:31 +0000

    objects python

---
+++ Developer Hired/Chapter 2 (end)/module_15/#suport_material.py
@@ -0,0 +1,29 @@
+# str
+
+nome = 'Ana Clara'
+
+print(nome.upper())
+print(nome.lower())
+print(nome.strip())
+print(nome.count('a'))
+print(nome.split(' '))
+print(nome.find('n'))
+print(nome.rfind('n'))
+print(nome.isnumeric())
+print(nome.replace('C','B'))
+
+print(nome)
+
+
+# tupla
+
+letras = ('a','c','g','c')
+print(letras.count('c'))
+print(letras.index('a'))
+
+
+# lista
+
+print()
+letras_lista = ('f','i','h','j')
+print(letras_lista)
