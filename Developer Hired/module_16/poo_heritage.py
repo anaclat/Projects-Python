29fbf9af5b6a95b52c7560b6e93f62a0a40f6263
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-07-09 13:04:48 +0000

    heritage in poo (python)

--- Developer Hired/module_16/poo_heritage.py
+++ Developer Hired/module_16/poo_heritage.py
@@ -3,3 +3,35 @@ class Pessoa:
     def __init__(self,nome,cpf):
         self.nome = nome
         self.cpf = cpf
+
+    def comer(self):
+        print(f'{self.nome} está comendo.')
+
+class Aluno(Pessoa):
+    
+    @staticmethod
+    def estudar():
+        print('O(A) aluno(a) está estudando.')
+
+class Professor(Pessoa):
+
+    @staticmethod
+    def ensinar():
+        print('O(A) professor(a) está ensinando.')
+
+
+aluno = Aluno(nome='Ana',cpf='181')
+print('Dados do aluno:')
+print(aluno.nome)
+print(aluno.cpf)
+aluno.comer()
+aluno.estudar()
+
+print('*'*50)
+
+professor = Professor(nome='Israel',cpf='094')
+print('Dados do professor:')
+print(professor.nome)
+print(professor.cpf)
+professor.comer()
+professor.ensinar()
