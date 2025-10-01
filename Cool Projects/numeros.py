6a4d86297de758809731179406ac3633aafee7d5
Ana Ronzani <aninhateixeira1810@gmail.com>
2025-10-01 17:09:49 +0000

    add adivinhador de numeros

--- Cool Projects/numeros.py
+++ Cool Projects/numeros.py
@@ -1,18 +1,22 @@
 from random import randint
 
-numero = randint(0,100)
+numero = randint(0,50)
 
 chute = int(input("Digite um número: "))
 
+tentativas = 0
+
 while True:
     if numero == chute:
-        print(f"Parabéns, você acertou! O número é {numero}.")
+        print(f"Parabéns, você acertou! O número é {numero}. Você acertou em {tentativas} tentativa(s).")
         break
 
     else:
      print(f"Chute errado. Tente novamente.")
      if numero > chute:
+        tentativas = tentativas + 1
         print("Digite um número maior.")
      elif numero < chute:
+        tentativas = tentativas + 1
         print("Digite um número menor.")
      chute = int(input("Digite um número: "))
